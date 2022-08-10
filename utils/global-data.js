4426dbc567cbc2bed0aac0b666b2ab36580505a1
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-08-10 10:39:12 +0700

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'hello';
+    : 'Rizky Ds';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Bakapedia Organisation Web Profile';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
